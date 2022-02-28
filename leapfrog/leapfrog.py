33eea58f208f8498c505b3f350f847cec37fc06c
pbenner <philipp.benner@bam.de>
2022-02-28 13:15:38 +0100

    2022/02/28-13:15:38 (Linux sv2110.zit.bam.de x86_64)

--- leapfrog/leapfrog.py
+++ leapfrog/leapfrog.py
@@ -31,37 +31,115 @@ from leapfrog_util import _leapfrog_regularize
 ## ----------------------------------------------------------------------------
 
 class Parameter(torch.nn.Parameter):
-    def __new__(cls, data, q: List[int], unique=False, weight_decay=None, proxop=None):
+    def __new__(cls, data, q: List[int], independent=True, unique=False, weight_decay=None, proxop=None):
         if q is not None and len(q) == 0:
             raise ValueError
         if q is None and weight_decay is None:
             raise ValueError
         if weight_decay is not None and len(weight_decay) != data.size(0):
-            raise ValueError
-        x          = torch.nn.Parameter.__new__(cls, data=data)
-        x.q        = q
-        x.data_old = torch.zeros (x.shape)
-        x.nu       = np.zeros(x.shape[1], dtype=np.float32)
-        x.sigma    = np.zeros(x.shape[1], dtype=np.float32)
-        x.proxop   = proxop
-        if weight_decay is None:
-            x.weight_decay = np.zeros(data.size(0), dtype=np.float32)
+            raise ValueError("Weight decay ")
+        if independent is False and unique is True:
+            raise ValueError("Unsupported combination of arguments")
+        x = torch.nn.Parameter.__new__(cls, data=data)
+        x.q           = q
+        x.data_old    = torch.zeros (x.shape)
+        x.proxop      = proxop
+        x.independent = independent
+        if independent:
+            x.nu    = np.zeros(x.shape[1], dtype=np.float32)
+            x.sigma = np.zeros(x.shape[1], dtype=np.float32)
+            if weight_decay is None:
+                x.weight_decay = np.zeros(data.size(0), dtype=np.float32)
+            else:
+                x.weight_decay = weight_decay
+            if unique:
+                x.exclude = np.array(x.shape[1]*[False])
+            else:
+                x.exclude = None
         else:
-            x.weight_decay = weight_decay
-        if unique:
-            x.exclude = np.array(x.shape[1]*[False])
-        else:
+            x.nu    = np.zeros(x.shape, dtype=np.float32).flatten()
+            x.sigma = np.zeros(x.shape, dtype=np.float32).flatten()
+            if weight_decay is None:
+                x.weight_decay = np.zeros(1, dtype=np.float32)
+            else:
+                x.weight_decay = weight_decay
             x.exclude = None
+
         return x
+
+    def regularize(self):
+        # initialize exclude tensor
+        if self.exclude is not None:
+            self.exclude.fill(False)
+        # synchronize cuda
+        if self.data.is_cuda:
+            torch.cuda.synchronize(device=self.data.device)
+        if self.independent:
+            for i in range(0, self.data.size(0)):
+                self._regularize_independent(i)
+        else:
+            self._regularize_global()
+
+    def _regularize_independent(self, i):
+        if self.q is not None:
+            if self.q[0] is None:
+                # do not regularize
+                return
+            if self.q[0] < 0 or self.q[0] >= self.data[i].size(0):
+                # do not regularize
+                return
+        # make a copy of the parameters (if using GPUs)...
+        data     = self.data    [i].cpu()
+        data_old = self.data_old[i].cpu()
+        grad     = self.grad    [i].cpu()
+        # update parameters
+        self.weight_decay[i] = _leapfrog_regularize(
+            data    .numpy(),
+            data_old.numpy(),
+            grad    .numpy(),
+            self.nu,
+            self.sigma,
+            self.exclude,
+            self.q[0],
+            self.proxop)
+        # copy result back
+        if self.data[i].is_cuda:
+            self.data[i].copy_(data)
+
+    def _regularize_global(self):
+        if self.q is not None:
+            if self.q[0] is None:
+                # do not regularize
+                return
+            if self.q[0] < 0 or self.q[0] >= self.data.flatten().size(0):
+                # do not regularize
+                return
+        # make a copy of the parameters (if using GPUs)...
+        data     = self.data    .cpu()
+        data_old = self.data_old.cpu()
+        grad     = self.grad    .cpu()
+        # update parameters
+        self.weight_decay = _leapfrog_regularize(
+            data    .flatten().numpy(),
+            data_old.flatten().numpy(),
+            grad    .flatten().numpy(),
+            self.nu,
+            self.sigma,
+            self.exclude,
+            self.q[0],
+            self.proxop)
+        # copy result back
+        if self.data.is_cuda:
+            self.data.copy_(data)
 
 ## Leapfrog linear layer
 ## ----------------------------------------------------------------------------
 
 class Linear(torch.nn.Module):
-    def __init__(self, in_features: int, out_features: int, q: int, unique=False, weight_decay=None, proxop=None, bias=True):
+    def __init__(self, in_features: int, out_features: int, q: int, independent=True, unique=False, weight_decay=None, proxop=None, bias=True):
         super().__init__()
         self.module        = torch.nn.Linear(in_features, out_features, bias=bias)
-        self.module.weight = Parameter(self.module.weight, q, unique=unique, weight_decay=weight_decay, proxop=proxop)
+        self.module.weight = Parameter(self.module.weight, q, independent=independent, unique=unique, weight_decay=weight_decay, proxop=proxop)
     def forward(self, *args, **kwargs):
         # Simply forward and args and kwargs to module
         return self.module(*args, **kwargs)
@@ -102,36 +180,7 @@ class Optimizer():
         for param_group in self.optimizer.param_groups:
             for parameters in param_group['params']:
                 if isinstance(parameters, Parameter):
-                    if parameters.exclude is not None:
-                        parameters.exclude.fill(False)
-                    # synchronize cuda
-                    if parameters.data.is_cuda:
-                        torch.cuda.synchronize(device=parameters.data.device)
-                    for i in range(0, parameters.data.size(0)):
-                        self._leapfrog_regularize(parameters, i)
-
-    def _leapfrog_regularize(self, parameters, i):
-        if parameters.q is not None:
-            if parameters.q[0] is None:
-                # do not regularize
-                return
-            if parameters.q[0] < 0 or parameters.q[0] >= parameters.data[i].size(0):
-                # do not regularize
-                return
-        # make a copy of the parameters (if using GPUs)...
-        param = parameters.data[i].cpu()
-        # update parameters
-        parameters.weight_decay[i] = _leapfrog_regularize(param.numpy(),
-            parameters.data_old[i].cpu().numpy(),
-            parameters.grad    [i].cpu().numpy(),
-            parameters.nu,
-            parameters.sigma,
-            parameters.exclude,
-            parameters.q[0],
-            parameters.proxop)
-        # copy result back
-        if parameters.data[i].is_cuda:
-            parameters.data[i].copy_(param)
+                    parameters.regularize()
 
     def converged(self, loss):
         converged = False
@@ -163,6 +212,15 @@ class Optimizer():
                 if isinstance(parameters, Parameter):
                     if len(parameters.q) > 1:
                         parameters.q = parameters.q[1:]
+                        finished = False
+        return finished
+
+    def is_final(self):
+        finished = True
+        for param_group in self.optimizer.param_groups:
+            for parameters in param_group['params']:
+                if isinstance(parameters, Parameter):
+                    if len(parameters.q) > 1:
                         finished = False
         return finished
 
